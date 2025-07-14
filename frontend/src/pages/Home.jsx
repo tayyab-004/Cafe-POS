eaa5e6ea889f273849cfc68889b71cf9e0488c8e
tayyab-004 <tayyabgee333@gmail.com>
2025-07-14 17:34:34 +0500

    Added MiniCard Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
 import BottomNav from "../components/shared/BottomNav";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
 
 const Home = () => {
   return (
@@ -7,6 +10,20 @@ const Home = () => {
       {/* Left Div */}
       <div className="flex-[3]">
         <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title="Total Earnings"
+            icon={<BsCashCoin />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title="In Progress"
+            icon={<GrInProgress />}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
       </div>
       {/* Right Div */}
       <div className="flex-[2] bg-[#1a1a1a]"></div>
