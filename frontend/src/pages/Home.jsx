d336cdfd1074b52e7acc3f8706ba59291f91f8d5
tayyab-004 <tayyabgee333@gmail.com>
2025-07-14 17:10:03 +0500

    Added Greeting Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import Greetings from "../components/home/Greetings";
 import BottomNav from "../components/shared/BottomNav";
 
 const Home = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* Left Div */}
-      <div className="flex-[3] bg-[#1a1a1a]"></div>
+      <div className="flex-[3]">
+        <Greetings />
+      </div>
       {/* Right Div */}
       <div className="flex-[2] bg-[#1a1a1a]"></div>
       <BottomNav />
