de57118306e103f3243bbcce945dd9fef75a4fbe
tayyab-004 <tayyabgee333@gmail.com>
2025-07-17 12:27:11 +0500

    Setup Middleware for globalErrorHandler

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 const app = express();
 
 const PORT = config.port;
 connectDB();
 
+// Root Endpoint
 app.get("/", (req, res) => {
   res.json({ message: "Working!" });
 });
 
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
