d891d857cf5c5699d2e43d85869c0a823ee848df
tayyab-004 <tayyabgee333@gmail.com>
2025-07-18 16:56:16 +0500

    Protected Routes Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,67 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom";
 import { Home, Auth, Orders, Tables, Menu } from "./pages";
 import Headers from "./components/shared/Headers";
+import { useSelector } from "react-redux";
 
 function Layout() {
   const location = useLocation();
   const hideHeaderRoutes = ["/auth"];
+  const { isAuth } = useSelector((state) => state.user);
 
   return (
     <>
       {!hideHeaderRoutes.includes(location.pathname) && <Headers />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/Menu" element={<Menu />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path="/auth" element={isAuth ? <Navigate to="/" /> : <Auth />} />
+        <Route
+          path="/orders"
+          element={
+            <ProtectedRoutes>
+              <Orders />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <ProtectedRoutes>
+              <Tables />
+            </ProtectedRoutes>
+          }
+        />
+        <Route
+          path="/Menu"
+          element={
+            <ProtectedRoutes>
+              <Menu />
+            </ProtectedRoutes>
+          }
+        />
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </>
   );
+}
+
+function ProtectedRoutes({ children }) {
+  const { isAuth } = useSelector((state) => state.user);
+  if (!isAuth) {
+    return <Navigate to="/auth" />;
+  }
+
+  return children;
 }
 
 function App() {
